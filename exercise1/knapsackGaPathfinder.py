5d80988cf4068e76c70d7a0c6966e1fb25ba53ad
P3akShadow <davidseka2000@gmail.com>
2024-11-02 17:42:25 +0100

    implemented working version for knapsack and ga

---
+++ exercise1/knapsackGaPathfinder.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+from knapsackUtils import evaluate, generateInstance
+
+from deap import base, creator, tools
+import random
+
+knapsackSizes = [10,10]
+itemSizes = [9,7,5,4,1]
+print(evaluate(knapsackSizes, itemSizes, range(5)))
+print(evaluate(knapsackSizes, itemSizes, [0,4,2,1,3]))
+
+knapsacks = 30
+items = 100
+(knapsackSizes, itemSizes) = generateInstance(knapsacks, items, knapsack_mean=5, items_mean=3)
+print(knapsackSizes)
+print(itemSizes)
+
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+toolbox.register("indices", random.sample, range(items), items)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
+
+#pxPartialyMathed uses Partially Matched Crossover
+#This means: Exchange a random sequence in the middle
+#for each exchanged element
+#Create a mapping that maps elements to the element in the same position but on the other side
+#map the occuring elements accordingly to keep the permutation property
+toolbox.register("mate", tools.cxPartialyMatched)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.1)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("evaluate", lambda x: [evaluate(knapsackSizes, itemSizes, x)])
+
+
+pop = [toolbox.individual() for _ in range(400)]
+invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+for ind, fit in zip(invalid_ind, fitnesses):
+    ind.fitness.values = fit
+best_ind = tools.selTournament(pop, 1, tournsize=len(pop))[0]
+starting_fitness = best_ind.fitness.values[0]
+
+best_ind
+for g in range(50):
+    print("#################")
+    print(f"generation {g}")
+    print("#################")
+    
+    best_ind = tools.selTournament(pop, 1, tournsize=len(pop))[0]
+    print(best_ind)
+    print(best_ind.fitness.values[0])
+
+    offspring = toolbox.select(pop, len(pop))
+    offspring = list(toolbox.map(toolbox.clone, offspring))
+
+    for child1, child2 in zip(offspring[::2], offspring[1::2]):  #[x::2] takes every other element starting from x
+        if random.random() < 0.8:
+            toolbox.mate(child1, child2)
+            del child1.fitness.values
+            del child1.fitness.values
+
+    for mutant in offspring:
+        if random.random() < 0.3:
+            toolbox.mutate(mutant)
+            del mutant.fitness.values
+
+    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+    
+    pop[:] = offspring
+
+print(f"sum elements {sum(itemSizes)}")
+print(f"sum knapsacks {sum(knapsackSizes)}")
+print(f"starting fitness was {starting_fitness}")
+print(f"last best fitness {best_ind.fitness.values[0]}")
--- exercise1/knapsackUtils.py
+++ exercise1/knapsackUtils.py
@@ -8,6 +8,17 @@ items_mean_default = 20.0
 def generateInstance(numberOfKnapsacks, numberOfItems, knapsack_mean=knapsack_mean_default, items_mean=items_mean_default):
     knapsacks = np.random.exponential(knapsack_mean, numberOfKnapsacks)
     items = np.random.exponential(items_mean, numberOfItems)
-
     return(knapsacks, items)
 
+def evaluate(knapsacks, items, order):
+    capacities = [size for size in knapsacks]
+
+    size = 0
+    for pos in order:
+        for i, capacity in enumerate(capacities):
+            if capacity >= items[pos]:
+                capacities[i] -= items[pos]
+                size += items[pos] * items[pos]
+                break
+
+    return size
