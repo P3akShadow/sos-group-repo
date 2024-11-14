2d2c071cdfcfe0f51c226cfc66e9efb764d261c8
David Seka <e11902064@student.tuwien.ac.at>
2024-11-14 16:58:26 +0100

    param tuning for tsp ga

--- exercise1/tspGaPathfinder.py
+++ exercise1/tspGaPathfinder.py
@@ -22,12 +22,12 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.in
 #Create a mapping that maps elements to the element in the same position but on the other side
 #map the occuring elements accordingly to keep the permutation property
 toolbox.register("mate", tools.cxPartialyMatched)
-toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.1)
-toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.03)
+toolbox.register("select", tools.selTournament, tournsize=8)
 toolbox.register("evaluate", lambda x: (evaluate(instance, x),))
 
 
-pop = [toolbox.individual() for _ in range(400)]
+pop = [toolbox.individual() for _ in range(40000)]
 invalid_ind = [ind for ind in pop if not ind.fitness.valid]
 fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
 for ind, fit in zip(invalid_ind, fitnesses):
@@ -36,7 +36,7 @@ best_ind = tools.selTournament(pop, 1, tournsize=len(pop))[0]
 starting_fitness = best_ind.fitness.values
 
 best_ind
-for g in range(50):
+for g in range(100):
     print("#################")
     print(f"generation {g}")
     print("#################")
@@ -49,13 +49,13 @@ for g in range(50):
     offspring = list(toolbox.map(toolbox.clone, offspring))
 
     for child1, child2 in zip(offspring[::2], offspring[1::2]):  #[x::2] takes every other element starting from x
-        if random.random() < 0.8:
+        if random.random() < 0.3:
             toolbox.mate(child1, child2)
             del child1.fitness.values
             del child1.fitness.values
 
     for mutant in offspring:
-        if random.random() < 0.3:
+        if random.random() < 0.7:
             toolbox.mutate(mutant)
             del mutant.fitness.values
 
